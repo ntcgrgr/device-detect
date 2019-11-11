2cbacec6ebbc833559e846b029d7b4a3a6fb305d
lucap <luc@rissc.de>
2019-11-11 13:59:22 +0100

    Added Dockerfile - why not Fixed Webpack build to actually use babel updated a npm dependency

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,17 @@ const path = require('path');
 
 module.exports = {
   entry: './src/index.js',
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        }
+      }
+    ]
+  },
   output: {
     filename: 'device-detect.js',
     path: path.resolve(__dirname, 'dist'),
