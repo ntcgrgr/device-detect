12faf9186191cda4454e7329611bb4229a494942
Luca Perna <12624410+MrMadClown@users.noreply.github.com>
2020-06-29 14:17:19 +0200

    Make it a package! (#4)
    * Added publish config.
    ran npm audit fix.
    updated .gitignore to ignore dist folder.
    removed dist folder
    
    * fixed name in package.json
    
    * Detect more Versions
    
    * Updated README

--- __tests__/determine-device.test.js
+++ __tests__/determine-device.test.js
@@ -1,7 +1,7 @@
 import testDataProvider from "./test-data-provider";
 import determineDevice from "../src/determine-device";
 
-Object.entries(testDataProvider).forEach(([name, {ua, device, browser}]) => {
+Object.entries(testDataProvider).forEach(([name, {ua, device}]) => {
   test(name, () => {
     expect(determineDevice(ua)).toBe(device);
   });
